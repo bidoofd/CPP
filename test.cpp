5542adab2815a9240f36c078fb6c8634b441af56
bidoofd <simplytran123@gmail.com>
2022-11-29 14:19:53 -0500

    change in test cpp

--- test.cpp
+++ test.cpp
@@ -26,3 +26,83 @@ int main()
         cerr << "Exception occured" << endl << e.what();
     }
 }
+
+/*
+
+#ifndef AGENT_H
+#define AGENT_H
+#include "Seller.h"
+#include "Buyer.h"
+#include <string>
+#include <list>
+
+// passing two args through contactSeller
+//  -contactedSeller to send information to and store
+//  -templist details for info to store to seller
+//      -not finished fully because going to work on seller
+
+using namespace std;
+
+struct listingDetails{
+    Property* ownedProperty;
+    double sellingPrice = 3;
+    string date;
+    bool soldStatus;
+};
+
+class Agent{
+    private:
+        Seller* contactedSeller;
+        Buyer* contactedBuyer;
+        listingDetails tempList;
+
+        double sellingPrice;
+        string date;
+        bool soldStatus;
+    public:
+        void recordOffer(listingDetails tempList);
+        void contactSeller(Seller *contactedSeller, listingDetails tempList);
+        void modifyListing(listingDetails tempList);
+        double getSellingPrice();
+};
+
+#endif
+
+*/
+
+/*
+
+#ifndef AGENT_H
+#define AGENT_H
+#include "Seller.h"
+#include "Buyer.h"
+#include <string>
+#include <list>
+
+using namespace std;
+
+struct listingDetails{
+    Property* ownedProperty;
+    double sellingPrice = 3;
+    string date;
+    bool soldStatus;
+};
+
+class Agent{
+    private:
+        Seller* contactedSeller;
+        Buyer* contactedBuyer;
+        list<listingDetails*> *listing = new list<listingDetails*>;
+    public:
+        void recordOffer(listingDetails tempList);
+        void contactSeller();
+        void modifyListing(listingDetails tempList);
+        double getSellingPrice();
+        //passed listing through function
+
+        //created getSellingPrice function()
+};
+
+#endif
+
+*/
