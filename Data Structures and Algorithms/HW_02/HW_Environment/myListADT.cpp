eaac3a782c4a397629463feae719d0d67c48d0cc
bidoofd <simplytran123@gmail.com>
2023-02-06 19:05:33 -0500

    fixed erase function and added counters for erase

--- Data Structures and Algorithms/HW_02/HW_Environment/myListADT.cpp
+++ Data Structures and Algorithms/HW_02/HW_Environment/myListADT.cpp
@@ -110,9 +110,9 @@ int myListADT::insert(int index, int object)
         }
 
         // * FOR LOOP
-        // 1 for setting a equal to pointer length
+        // 1 for setting variable a equal to pointer length
         // 1 for accessing pointer length
-        // 1 for comparing a to the index
+        // 1 for comparing variable a to the index
         // 1 for a--
 
         // +4
@@ -164,7 +164,7 @@ int myListADT::insert(int index, int object)
         countOperations = countOperations + 7;
 
         cout << "The number of iterations that happened in the loop is: " << countIterations << "." << endl;
-        cout << "The number of primitive operations that happened is: " << countInstructions << "." << endl;
+        cout << "The number of primitive operations that happened is: " << countOperations << "." << endl;
 
         return this->pointer;
     }
@@ -177,33 +177,101 @@ int myListADT::erase(int index)
 {
     // Variables for counts
     int countIterations = 0;
-    int countInstructions = 0;
+    int countOperations = 0;
 
     // If the pointer size is 0, then returns -2
     if(this->pointer == 0)
     {
+        // Adding 4
+        //  - 1 for accessing pointer length
+        //  - 1 for accessing length
+        //  - 1 for comparing pointer length and length
+        //  - 1 for returning a value
+        countOperations = countOperations + 4;
         return -2;
     }
 
     // Checks to see if the index exists
     if(index >= 0 && index <= this->pointer)
     {
+        // Adding 4
+        //  - 1 for accessing pointer length
+        //  - 1 for comparing index to 0
+        //  - 1 for comparing index to pointer length
+        //  - 1 for comparing both flags 
+        countOperations = countOperations + 4;
+
         // Stores the erasing value in a variable
         int erasedNum = this->array[index];
+
+        // Adding 3
+        //  - 1 for accessing array
+        //  - 1 for accessing array index
+        //  - 1 for assigning value to erasedNum
+
+        countOperations = countOperations + 3;
+
         // For loop to shift elements to the left
-        for(int a = index; a > index; a--)
+        for(int a = index; a < this->pointer; a++)
         {
-            this->array[a] = this->array[a-1];
+            this->array[a] = this->array[a+1];
         }
 
+        // * FOR LOOP
+        // 1 for setting variable a equal to index
+        // 1 for comparing a to the index
+        // 1 for a--
+
+        // +4
+
+        // 1 for comparison
+        // 1 for a++
+
+        // +(2*n-1)
+
+        // 1 for subtracting
+
+        // +1
+        // * END FOR LOOP
+
+        // ** INSIDE FOR LOOP
+        // 1 for accessing array
+        // 1 for accessing array value
+        // 1 for accessing array again
+        // 1 for accessing array value before
+        // 1 for assigning current array value for previous
+
+        // +5
+
+        // 1 for adding to countIterations
+        // 1 for assigning new countIterations value
+
+        // +2
+        // ** END INSIDE FOR LOOP
+
         // Decreases pointer size
+
+        countOperations = countOperations + 5 + (2 * this->pointer - 1);
+
         this->pointer = this->pointer - 1;
 
+        // 1 for accessing pointer
+        // 1 for accessing pointer again
+        // 1 for adding to pointer
+        // 1 for assigning old pointer with new
+
+        countOperations = countOperations + 4;
+
         // return erased value
+
+        cout << "The number of iterations that happened in the loop is: " << countIterations << "." << endl;
+        cout << "The number of primitive operations that happened is: " << countOperations << "." << endl;
+
         return erasedNum;
     }
 
     // return -1 if index does not exist
+
     return -1;
 }
 
