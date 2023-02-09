cd808f77638b4790b1366cc3e62d944d582899fc
bidoofd <simplytran123@gmail.com>
2023-02-09 14:40:47 -0500

    comments for header

--- Data Structures and Algorithms/HW_02/HW_Environment/myListADT.h
+++ Data Structures and Algorithms/HW_02/HW_Environment/myListADT.h
@@ -7,11 +7,29 @@ using namespace std;
 class myListADT
 {
     private:
+        // Variables
+        //  - name: name for object
+        //  - pointer: size of current array
+        //  - length: size of actual array
+        //  - *array: dynamically creates array based on length
+
         string name;
         int pointer = 0;
         int length = 0;
         int *array = new int[length];
     public:
+        // Methods
+        //  - myListADT(): creates the myListADT object
+        //  - isEmpty(): checks to see if array in object is empty
+        //  - size(): returns size of the current array (not max size)
+        //  - at(): returns the value of the inputted index
+        //  - set(): changes the value at the inputted index with the inputted object
+        //  - insert(): inserts a value at the inputted index with the inputted object
+        //      - it also shifts the elements to keep order
+        //  - erase(): erases the value at the inputted index and shifts elements to keep order
+        //  - printList(): prints the values inside the array of the object
+        //  - writeOutput(): writes to file for tracing
+
         myListADT(string name, int size);
         int isEmpty();
         int size();
